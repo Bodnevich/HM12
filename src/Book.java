2eb89d45bee13a8f70760db13dc6658d6c04fde4
Bodnevich <bodnevichkristina@mail.ru>
2025-08-19 17:00:21 +0300

    HM12 испр

---
+++ src/Book.java
@@ -0,0 +1,24 @@
+public class Book {
+    private String name;
+    private int yearOfPublication;
+    private Author author;
+
+    public Book(String name, int yearOfPublication, Author author) {
+        this.name = name;
+        this.yearOfPublication = yearOfPublication;
+        this.author = author;
+    }
+
+    public String getName () {
+        return this.name;
+    }
+
+    public int getYearOfPublication() {
+        return this.yearOfPublication;
+    }
+
+    public Author getAuthor() {
+        return this.author;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Author author = new Author("Стивен", "Кинг");
+        Author author1 = new Author("Чак", "Паланик");
+        Book book = new Book("Спящие красавицы", 2017, author);
+        Book book1 = new Book("Снафф", 2015, author1);
+        System.out.println("Название книги - " + book.getName() + ", год публикации: " + book.getYearOfPublication() + ", автор: " + author.getFirstName() + " " + author.getLastName());
+        System.out.println("Название книги - " + book1.getName() + ", год публикации: " + book1.getYearOfPublication() + ", автор: " + author1.getFirstName() + " " + author1.getLastName());
     }
 }
