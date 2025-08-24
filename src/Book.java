5e5eb2652dd1b0d46b71ee7c581da62128b41eb5
Bodnevich <bodnevichkristina@mail.ru>
2025-08-24 17:16:29 +0300

    HM13

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private String name;
     private int yearOfPublication;
@@ -28,5 +30,17 @@ public class Book {
     @Override
     public String toString() {
         return name + " " + author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearOfPublication == book.yearOfPublication && Objects.equals(name, book.name) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, yearOfPublication, author);
     }
 }
